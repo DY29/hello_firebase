81d4e40b19addfc008ee241d68652b26b7b07977
DY29 <kwondy2801@gmail.com>
2021-02-07 05:26:14 +0900

    Database#2:Insert

--- public/assets/js/auth.js
+++ public/assets/js/auth.js
@@ -80,3 +80,44 @@ function signup()
 }
 
 
+function facebooklogin() {
+    var user = firebase.auth().currentUser;
+    
+    if (user) {
+        //
+    } else {
+    
+        var provider = new firebase.auth.FacebookAuthProvider();
+        firebase.auth().signInWithPopup(provider).then(function (result) {
+            // This gives you a Facebook Access Token.
+            // var token = result.credential.accessToken;
+            // The signed-in user info.
+            // var user = result.user;
+            console.log("facebook connected");
+        }).catch(function (error) {
+            console.log(error);
+        });
+    }
+}
+
+
+function googlelogin(){
+
+    var user = firebase.auth().currentUser;
+    if (user) {
+    //
+    } else {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/plus.login');
+    firebase.auth().signInWithPopup(provider).then(function (result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        var user = result.user;
+        console.log("connected");
+        
+    }).catch(function (error) {
+        console.log('error',error);
+    });
+    }
+
+}
